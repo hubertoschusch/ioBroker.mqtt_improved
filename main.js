b3ffffac01a2b8a2cc4c308faebc7b9e6544e072
hetzendorfer <dominik.hetzendorfer@gmail.com>
2022-01-26 16:36:49 +0100

    .

--- main.js
+++ main.js
@@ -1,167 +1,215 @@
-"use strict";
-
-/*
- * Created with @iobroker/create-adapter v2.0.2
+/**
+ *
+ *      ioBroker mqtt Adapter
+ *
+ *      (c) 2014-2021 bluefox <dogafox@gmail.com>
+ *
+ *      MIT License
+ *
  */
+'use strict';
 
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
-const utils = require("@iobroker/adapter-core");
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
+const adapterName = require('./package.json').name.split('.').pop();
+let adapter;
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+let server = null;
+let client = null;
+let states = {};
 
-class MqttImproved extends utils.Adapter {
+const messageboxRegex = new RegExp('\\.messagebox$');
 
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "mqtt_improved",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
-
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
-
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
-
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
-
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
-
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
-	}
-
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
+function decrypt(key, value) {
+    let result = '';
+    for (let i = 0; i < value.length; ++i) {
+        result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
+    }
+    return result;
 }
 
-if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new MqttImproved(options);
+function startAdapter(options) {
+    options = options || {};
+    Object.assign(options, {name: adapterName});
+
+    adapter = new utils.Adapter(options);
+
+    adapter.on('message', obj =>
+        obj && processMessage(obj));
+
+    adapter.on('ready', () => {
+        adapter.config.pass = decrypt('Zgfr56gFe87jJOM', adapter.config.pass);
+        adapter.config.maxTopicLength = adapter.config.maxTopicLength || 100;
+        if (adapter.config.ssl && adapter.config.type === 'server') {
+            // Load certificates
+            adapter.getCertificates((err, certificates) => {
+                adapter.config.certificates = certificates;
+                main();
+            });
+        } else {
+            // Start
+            main();
+        }
+    });
+
+    adapter.on('unload', () => {
+        client && client.destroy();
+        server && server.destroy();
+    });
+
+    // is called if a subscribed state changes
+    adapter.on('stateChange', (id, state) => {
+        adapter.log.debug(`stateChange ${id}: ${JSON.stringify(state)}`);
+        // State deleted
+        if (!state) {
+            delete states[id];
+            // If SERVER
+            if (server) server.onStateChange(id);
+            // if CLIENT
+            if (client) client.onStateChange(id);
+        } else
+        // you can use the ack flag to detect if state is desired or acknowledged
+        if ((adapter.config.sendAckToo || !state.ack) && !messageboxRegex.test(id)) {
+            const oldVal = states[id] ? states[id].val : null;
+            const oldAck = states[id] ? states[id].ack : null;
+            states[id] = state;
+
+            // If value really changed
+            if (!adapter.config.onchange || oldVal !== state.val || oldAck !== state.ack) {
+                // If SERVER
+                server && server.onStateChange(id, state);
+                // if CLIENT
+                client && client.onStateChange(id, state);
+            }
+        }
+    });
+    return adapter;
+}
+
+function processMessage(obj) {
+    if (!obj || !obj.command) return;
+    switch (obj.command) {
+        case 'sendMessage2Client':
+            if (server) {
+                adapter.log.debug(`Sending message from server to clients via topic ${obj.message.topic}: ${obj.message.message} ...`);
+                server.onMessage(obj.message.topic, obj.message.message);
+            } else if (client) {
+                adapter.log.debug(`Sending message from client to server via topic ${obj.message.topic}: ${obj.message.message} ...`);
+                client.onMessage(obj.message.topic, obj.message.message);
+            } else {
+                adapter.log.debug(`Neither MQTT server nor client not started, thus not sending message via topic ${obj.message.topic} (${obj.message.message}).`);
+            }
+            break;
+
+        case 'sendState2Client':
+            if (server) {
+                adapter.log.debug(`Sending message from server to clients ${obj.message.id}: ${obj.message.state} ...`);
+                server.onStateChange(obj.message.id, obj.message.state);
+            } else if (client) {
+                adapter.log.debug(`Sending message from client to server ${obj.message.id}: ${obj.message.state} ...`);
+                client.onStateChange(obj.message.id, obj.message.state);
+            } else {
+                adapter.log.debug(`Neither MQTT server nor client not started, thus not sending message to client ${obj.message.id} (${obj.message.state}).`);
+            }
+            break;
+
+        case 'test': {
+            // Try to connect to mqtt broker
+            if (obj.callback && obj.message) {
+                const mqtt = require('mqtt');
+                const _url = `mqtt://${obj.message.user ? (`${obj.message.user}:${obj.message.pass}@`) : ''}${obj.message.url}${obj.message.port ? (':' + obj.message.port) : ''}?clientId=ioBroker.${adapter.namespace}`;
+                const _client = mqtt.connect(_url);
+                // Set timeout for connection
+                const timeout = setTimeout(() => {
+                    _client.end();
+                    adapter.sendTo(obj.from, obj.command, 'timeout', obj.callback);
+                }, 2000);
+
+                // If connected, return success
+                _client.on('connect', () => {
+                    _client.end();
+                    clearTimeout(timeout);
+                    adapter.sendTo(obj.from, obj.command, 'connected', obj.callback);
+                });
+                // If connected, return success
+                _client.on('error', (err) => {
+                    _client.end();
+                    clearTimeout(timeout);
+                    adapter.log.warn(`Error on mqtt test: ${err}`);
+                    adapter.sendTo(obj.from, obj.command, 'error', obj.callback);
+                });
+            }
+        }
+    }
+}
+
+let cnt = 0;
+function readStatesForPattern(pattern) {
+    adapter.getForeignStates(pattern, (err, res) => {
+        if (!err && res) {
+            states = states || {};
+
+            Object.keys(res).filter(id => !messageboxRegex.test(id))
+                .forEach(id => states[id] = res[id]);
+        }
+        // If all patters answered, start client or server
+        if (!--cnt) {
+            if (adapter.config.type === 'client') {
+                client = new require('./lib/client')(adapter, states);
+            } else {
+                server = new require('./lib/server')(adapter, states);
+            }
+        }
+    });
+}
+
+function main() {
+    adapter.config.forceCleanSession = adapter.config.forceCleanSession || 'no'; // default
+
+    // Subscribe on own variables to publish it
+    if (adapter.config.publish) {
+        const parts = adapter.config.publish.split(',');
+        for (let t = 0; t < parts.length; t++) {
+            if (parts[t].indexOf('#') !== -1) {
+                adapter.log.warn(`Used MQTT notation for ioBroker in pattern "${parts[t]}": use "${parts[t].replace(/#/g, '*')} notation`);
+                parts[t] = parts[t].replace(/#/g, '*');
+            }
+            adapter.subscribeForeignStates(parts[t].trim());
+            cnt++;
+            readStatesForPattern(parts[t]);
+        }
+    } else {
+        // subscribe for all variables
+        adapter.subscribeForeignStates('*');
+        readStatesForPattern('*');
+    }
+
+    adapter.config.defaultQoS = parseInt(adapter.config.defaultQoS, 10) || 0;
+    adapter.config.retain = adapter.config.retain === 'true' || adapter.config.retain === true;
+    adapter.config.persistent = adapter.config.persistent === 'true' || adapter.config.persistent === true;
+    adapter.config.retransmitInterval = parseInt(adapter.config.retransmitInterval, 10) || 2000;
+    adapter.config.retransmitCount = parseInt(adapter.config.retransmitCount, 10) || 10;
+
+    if (adapter.config.retransmitInterval < adapter.config.sendInterval) {
+        adapter.config.retransmitInterval = adapter.config.sendInterval * 5;
+    }
+    adapter.EXIT_CODES = utils.EXIT_CODES || {
+        ADAPTER_REQUESTED_TERMINATION: 11
+    };
+
+    // If no subscription, start client or server
+    if (!cnt) {
+        if (adapter.config.type === 'client') {
+            client = new require(__dirname + '/lib/client')(adapter, states);
+        } else {
+            server = new require(__dirname + '/lib/server')(adapter, states);
+        }
+    }
+}
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;
 } else {
-	// otherwise start the instance directly
-	new MqttImproved();
+    // or start the instance directly
+    startAdapter();
 }
