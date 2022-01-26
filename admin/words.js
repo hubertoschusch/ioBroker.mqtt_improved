f5c4c16b94acef6051810737a3284715248f9820
hetzendorfer <dominik.hetzendorfer@gmail.com>
2022-01-26 16:52:35 +0100

    .

--- admin/words.js
+++ admin/words.js
@@ -1,41 +1,41 @@
 /*global systemDictionary:true */
-"use strict";
+'use strict';
 
 systemDictionary = {
-	"mqtt_improved adapter settings": {
-		"en": "Adapter settings for mqtt_improved",
-		"de": "Adaptereinstellungen für mqtt_improved",
-		"ru": "Настройки адаптера для mqtt_improved",
-		"pt": "Configurações do adaptador para mqtt_improved",
-		"nl": "Adapterinstellingen voor mqtt_improved",
-		"fr": "Paramètres d'adaptateur pour mqtt_improved",
-		"it": "Impostazioni dell'adattatore per mqtt_improved",
-		"es": "Ajustes del adaptador para mqtt_improved",
-		"pl": "Ustawienia adaptera dla mqtt_improved",
-		"zh-cn": "mqtt_improved的适配器设置"
-	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
-	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "вариант 2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
-	}
+    'mqtt-improved adapter settings': {
+        'en': 'Adapter settings for mqtt-improved',
+        'de': 'Adaptereinstellungen für mqtt-improved',
+        'ru': 'Настройки адаптера для mqtt-improved',
+        'pt': 'Configurações do adaptador para mqtt-improved',
+        'nl': 'Adapterinstellingen voor mqtt-improved',
+        'fr': "Paramètres d'adaptateur pour mqtt-improved",
+        'it': "Impostazioni dell'adattatore per mqtt-improved",
+        'es': 'Ajustes del adaptador para mqtt-improved',
+        'pl': 'Ustawienia adaptera dla mqtt-improved',
+        'zh-cn': 'mqtt-improved的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
 };
