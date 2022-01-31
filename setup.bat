2589bf0367a0e8dd2fcc86b8caf04d8bf42129b2
hetzendorfer <dominik.hetzendorfer@gmail.com>
2022-01-31 20:06:10 +0100

    .

--- setup.bat
+++
@@ -1,9 +0,0 @@
-cd "C:\Program Files\iobroker\adapter\"
-del iobroker.mqtt_improved-*.tgz
-cd "C:\Users\Dominik\Desktop\Diplomarbeit\working\ioBroker.mqtt_improved"
-call npm pack
-move /y iobroker.mqtt_improved-*.tgz "C:\Program Files\iobroker\adapter\"
-cd "C:\Program Files\iobroker\SmartHome"
-set /p var=
-call npm i "..\adapter\iobroker.mqtt_improved-0.0.2.tgz"
-pause
